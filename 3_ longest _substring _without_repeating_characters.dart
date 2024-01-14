ad62e8675a0651573dd771a5c2e09009a6e8eaf9
Akib Siddik <akib.siddiki@gmail.com>
2024-01-14 09:14:51 +0600

    solving 3

--- 3_ longest _substring _without_repeating_characters.dart
+++ 3_ longest _substring _without_repeating_characters.dart
@@ -1,5 +1,5 @@
 void main() {
-  print(Solution().lengthOfLongestSubstring('dvdf'));
+  print(Solution().lengthOfLongestSubstring('jbpnbwwd'));
 }
 
 class Solution {
@@ -8,15 +8,19 @@ class Solution {
     int lenght = 0;
 
     for (int i = 0; i < s.length; i++) {
+      // print(i);
       if (subString.contains(s[i])) {
         if (lenght < subString.length) {
           lenght = subString.length;
-          subString = '${s[i]}';
+          // print(subString.indexOf(s[i]));
           i = subString.indexOf(s[i]);
+          subString = '';
+          // print(i);
         }
       } else {
         subString += s[i];
       }
+      print(subString);
     }
 
     lenght = lenght < subString.length ? subString.length : lenght;
